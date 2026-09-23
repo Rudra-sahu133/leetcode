136b159df8e67e77456b141c15441415fe427609
Rudra-sahu133 <abdvilliers133@gmail.com>
2026-09-23 16:32:44 +0530

    Time: 3 ms (71.49%), Space: 102.4 MB (38.48%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
-    int find (vector<int> &pf , int l , int r , int p) {
-        while (l<=r) {
-            int m = l + (r-l)/2 ;
-            if (pf[m] == p) return m;
-            else if (pf[m] < p){
-                l = m + 1 ;
-            }
-            else {
-                r = m - 1 ;
-            }
-        }
-        return -1 ;
-    }
+    
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size() ;
-        vector<int> pf(n) ;
-        // vector<int> sf(n) ;
-        pf[0] = nums[0];
-        // sf[n-1] = nums[n-1];
-        for (int i=1 ; i<n ; i++){
-            pf[i] = pf[i-1] + nums[i];
+        int sum =0 ;
+        for (int i=0 ; i<n ; i++) {
+            sum += nums[i] ;
         }
-        // for (int i=n-2 ; i>=0 ; i--) {
-        //     sf[i]  = sf[i+1] + nums[i];
-        // }
-        int ans = INT_MAX ;
-        int idx = find(pf , 0 , n-1 , x);
-        if (idx != -1){
-            ans = min(ans , idx + 1) ;
+        sum = sum - x ;
+        if (sum < 0) return -1;
+        if (sum == 0) {
+            return n ;
         }
         int curr= 0;
-        for (int i=n-1 ; i >=0  ; i-- ) {
-            curr += nums[i] ;
-            if (curr == x){
-                ans = min(ans , n-i);
+        int l =0 ;
+        int r = 0;
+        int ans = 0;
+        while (l < n && r < n ) {
+            if (curr < sum){
+                curr += nums[r];
+                r++;
             }
-            if (curr > x) break;
-            int idx = find(pf , 0 , i-1 , x - curr) ;
-            if (idx != -1){
-                ans = min(ans , n-i + idx  + 1) ;
+            if (curr == sum) {
+                ans = max(ans , r-l) ;
+                
+                curr = curr - nums[l];
+                l++;
             }
+            
+                while (l < r && curr > sum  ){
+                    curr = curr - nums[l];
+                    l++;
+                }
+            
         }
-        if (ans == INT_MAX) return -1;
-        return ans ;
+        if (curr == sum) {
+            ans = max(ans , r-l) ;
+
+        }
+        if (ans == 0) return -1;
+        return n - ans ; 
+
+
     }
 };
